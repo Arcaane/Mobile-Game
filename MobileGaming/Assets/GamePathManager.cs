e4de7e9c4349e71ead2b51e02f858ef0ee067ddb
Arcaane <cordaurelien@gmail.com>
2023-04-15 16:54:38 +0200

    pull depuis l'ocean atlantique

---
+++ MobileGaming/Assets/GamePathManager.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class GamePathManager : MonoBehaviour
+{
+    private static GamePathManager Instance;
+    public static GamePathManager instance
+    {
+        get => Instance;
+        set => Instance = value;
+    }
+    
+    public LevelOpener[] levels;
+    public int unlockedLevels = 1;
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+
+    void Start()
+    {
+        for (int i = levels.Length - 1; i >= 0; i--)
+        {
+            levels[i].levelIndex = levels.Length - i;
+        }
+    }
+}
