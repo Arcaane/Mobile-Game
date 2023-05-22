b0efab5f78816ca33ffe9317c9056fb0424962d6
ETU\h.ramiandrasoa <h.ramiandrasoa@rubika-edu.com>
2023-05-22 15:08:51 +0200

    Added eventmanager

---
+++ MobileGaming/Assets/Scripts/Compositor/AddressableSingleton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using static UnityEngine.AddressableAssets.Addressables;
+
+public abstract class AddressableSingleton<T> : MonoBehaviour where T : AddressableSingleton<T>
+{
+    public static T Instance => instance != null ? instance : LoadInstance();
+    private static T instance;
+
+    private static T LoadInstance()
+    {
+        var op = LoadAssetAsync<GameObject>($"{typeof(T)}");
+        instance = op.WaitForCompletion().GetComponent<T>();
+        return instance;
+    }
+}
