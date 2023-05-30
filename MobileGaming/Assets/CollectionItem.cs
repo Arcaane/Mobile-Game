01e8997b6a2347dc7c7cdf9f54c4466a864830b3
ETU\h.ramiandrasoa <h.ramiandrasoa@rubika-edu.com>
2023-05-30 16:42:54 +0200

    Fixed conflicts

--- MobileGaming/Assets/CollectionItem.cs
+++ MobileGaming/Assets/CollectionItem.cs
@@ -10,7 +10,7 @@ public class CollectionItem : ScriptableObject
     [field: SerializeField] public ItemRarity Rarity { get; private set; }
     [ResizableTextArea] public string descriptionText;
     [ResizableTextArea] public string powerUpText;
-    [field: SerializeField] public ScriptableItemEffect ScriptableItemEffect { get; private set; }
+    [field: SerializeField] public List<ScriptableItemEffect> ScriptableItemEffects { get; private set; }
     [field: SerializeField] private List<ItemFragment> fragments = new List<ItemFragment>();
     public List<ItemFragment> Fragments => fragments;
     public bool isEquiped;
