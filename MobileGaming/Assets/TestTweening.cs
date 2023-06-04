af3b3ad31bf21e7362522f6d75cc584ca5a6d543
Arcaane <cordaurelien@gmail.com>
2023-06-04 15:57:27 +0200

    uwu

--- MobileGaming/Assets/TestTweening.cs
+++ MobileGaming/Assets/TestTweening.cs
@@ -13,36 +13,53 @@ public class TestTweening : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        initPos = new Vector3[goldPile.childCount];
+        initRot = new Quaternion[goldPile.childCount];
+
         for (int i = 0; i < goldPile.childCount; i++)
         {
-            initPos[i] = goldPile.GetChild(i).position;
-            initRot[i] = goldPile.GetChild(i).rotation;
+            initPos[i] = goldPile.GetChild(i).transform.position;
+            initRot[i] = goldPile.GetChild(i).transform.rotation;
         }
     }
 
     [ContextMenu("OUGAA")]
-    public void Button(int i)
+    public void Button()
     {
         Reset();
  
         float delay = 0;
-        goldPile.gameObject.SetActive(true);
-        
+
         for (int j = 0; j < goldPile.childCount; j++)
         {
-            goldPile.GetChild(i).DOScale(1f, 0.335f).SetDelay(delay).SetEase(Ease.OutBack);
-            goldPile.GetComponent<RectTransform>().DOAnchorPos(new Vector2(-103, 349), 1.2f).SetDelay(delay + 0.5f).SetEase(Ease.OutBack);
-            goldPile.GetChild(i).DOScale(0, 0.3f).SetDelay(delay + 1f).SetEase(Ease.OutBack);
-            delay += 0.2f;
+            var objectTween = goldPile.GetChild(j);
+            objectTween.DOScale(1f, 0.3f).SetDelay(delay).SetEase(Ease.OutBack);
+            objectTween.GetComponent<RectTransform>().DOAnchorPos(new Vector2(-154.5f, 349), 0.8f).SetDelay(delay + 0.5f).SetEase(Ease.InBack);
+            objectTween.DORotate(Vector3.zero, 0.5f).SetDelay(delay + 0.5f).SetEase(Ease.Flash);
+            objectTween.DOScale(0, 0.3f).SetDelay(delay + 1.8f).SetEase(Ease.OutBack);
+            delay += 0.1f;
         }
+
+        StartCoroutine(GetGoldText(10));
     }
 
     public void Reset()
     {
-        for (int i = 0; i < goldPile.childCount; i++)
+        for (int i = 0; i < goldPile.childCount; i++) 
         {
-            goldPile.GetChild(i).position = initPos[i];
-            goldPile.GetChild(i).rotation = initRot[i];
+            goldPile.GetChild(i).transform.position = initPos[i];
+            goldPile.GetChild(i).transform.rotation = initRot[i];
+        }
+    }
+
+    IEnumerator GetGoldText(int coinCount)
+    {
+        yield return new WaitForSecondsRealtime(0.8f);
+
+        var timer = 0.05f;
+        for (int i = 0; i < coinCount; i++)
+        {
+            yield return new WaitForSecondsRealtime(timer);
         }
     }
 }
