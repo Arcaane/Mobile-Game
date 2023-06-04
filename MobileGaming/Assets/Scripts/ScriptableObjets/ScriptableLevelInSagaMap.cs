542f6859564786f8eabd1da282903c491c4cd824
ETU\h.ramiandrasoa <h.ramiandrasoa@rubika-edu.com>
2023-06-04 19:32:12 +0200

    Autoscroll to top unlocked level

--- MobileGaming/Assets/Scripts/ScriptableObjets/ScriptableLevelInSagaMap.cs
+++ MobileGaming/Assets/Scripts/ScriptableObjets/ScriptableLevelInSagaMap.cs
@@ -20,7 +20,7 @@ public class ScriptableLevelInSagaMap : ScriptableObject
     [field:Header("Navigation")]
     [field: SerializeField,Scene] public int LevelScene { get; private set; }
     [field: SerializeField,Scene] public int NextLevelScene { get; private set; }
-    [field: SerializeField] public Vector3 autoscrollPosition { get; private set; }
+    [field: SerializeField] public float autoscrollPositionY { get; private set; }
 
     [Space(5)] [Header("Level Selection Section")]
     public int gearCountPlayerCanEquip;
